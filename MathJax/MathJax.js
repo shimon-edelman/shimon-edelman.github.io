8951f78f8b3568aebe6ac1664413659abd92ece9
Shimon Edelman <se37@cornell.edu>
2021-07-29 12:41:34 -0400

    fixed MathJax

---
+++ MathJax/MathJax.js
@@ -0,0 +1,5 @@
+  /Users/shimon/Sites/shimon-edelman.github.io/MathJax:
+  total used in directory 120 available 4825359217
+  drwxr-xr-x    3 shimon  admin     96 Jul 14 11:53 .
+  drwxr-xr-x  166 shimon  admin   5312 Jul 29 12:37 ..
+  -rw-r--r--    1 shimon  admin  57499 Jul 14 11:53 MathJax.js?config=TeX-AMS-MML_HTMLorMML
